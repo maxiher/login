a6acfdc0145f6a1529cbbd5439bcd8a78ce3580b
Felipe Barceló Vilaró <101574184+felipebarcelo@users.noreply.github.com>
2025-08-14 20:08:12 -0300

    Update validacion.js

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,16 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('regBtn').addEventListener('click', function (e) {
+    const check = document.getElementById('terminos');
+    if (!check.checked) {
+      e.preventDefault();
+      document.getElementById('alert-danger').classList.add('show');
+      return;
+    }
+    document.getElementById('alert-success').classList.add('show');
+    
+  });
+});
