05b5abb798064feca0cfadcc0ad93d5d36d815a9
Vale-1610 <valemar1610@gmail.com>
2025-08-14 18:55:30 -0300

    Agrego contraseña de menos de 6 caracteres y contraseña y repetir contraseña iguales

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,24 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+
+document.getElementById("regBtn").addEventListener("click", () => {
+    document.getElementById("alert-success").classList.remove("show");
+    document.getElementById("alert-danger").classList.remove("show");
+
+    const pass1 = document.getElementById("password1").value;
+    const pass2 = document.getElementById("password2").value;
+
+    // La contraseña debe tener al menos 6 caracteres
+    const largoContrasenia = pass1.length >= 6;
+    // Los datos ingresados en "Contraseña" y "Repetir contraseña" deben ser iguales.
+    const contraseniasIgual = pass1 === pass2;
+
+    // Ver si se cumplen las dos condiciones
+    if (largoContrasenia && contraseniasIgual) {
+        showAlertSuccess();
+    } else {
+        showAlertError();
+    }
+});
