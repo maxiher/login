2c4bc8c66058311d75f3b40d6c10928a7be68001
MaxiHer <maxihernandez444@gmail.com>
2025-08-15 13:30:12 -0300

    final

--- js/validacion.js
+++ js/validacion.js
@@ -39,3 +39,20 @@ document.getElementById("regBtn").addEventListener("click", () => {
         showAlertError();
     }
 });
+
+document.getElementById("regBtn").addEventListener("click", function () {
+
+  let nombre = document.getElementById("nombre").value.trim();
+  let apellido = document.getElementById("apellido").value.trim();
+  let email = document.getElementById("email").value.trim();
+  let password1 = document.getElementById("password1").value.trim();
+  let password2 = document.getElementById("password2").value.trim();
+  let terminos = document.getElementById("terminos").checked; 
+
+  if (nombre === "" || apellido === "" || email === "" || password1 === "" || password2 === "") {
+      showAlertError("Por favor completa todos los campos.");
+      return;
+  } else {
+      showAlertSuccess();
+    }
+  })
